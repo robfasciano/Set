4b959ba7f2c8ea1ab73101e1a71ba3cad4252bdb
Robert Fasciano <rob.fasciano@yahoo.com>
2024-10-21 07:19:39 -0400

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Robert Fasciano on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
